98a7f29144080c128cdc62cbc8f238be10521b0c
TitiOS <t-o-silva@hotmail.com>
2024-10-24 19:15:30 -0300

    testando

---
+++ src/app/about/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "../page.module.css";
+
+export default function About() {
+  return (
+    <main>
+        <h1>TENTANDO APRENDER ESSA BAGAÇA</h1>
+    </main>
+  );
+}
